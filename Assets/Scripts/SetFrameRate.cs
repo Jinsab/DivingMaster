2bca8f5017971b441929c30e4801ca13b226046d
Jinsab <b01077016039@gmail.com>
2020-11-24 17:11:07 +0900

    프레임 60 고정

---
+++ Assets/Scripts/SetFrameRate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFrameRate : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
