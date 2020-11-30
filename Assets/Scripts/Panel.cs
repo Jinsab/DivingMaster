38a62db3acc3707037b0cd20cb39782211b58510
Jinsab <b01077016039@gmail.com>
2020-11-30 14:44:19 +0900

    패널 스크립트 확장성 추가

--- Assets/Scripts/Panel.cs
+++ Assets/Scripts/Panel.cs
@@ -7,14 +7,19 @@ using UnityEngine.UI;
 public class Panel : MonoBehaviour, IPointerDownHandler
 {
 	public StartGame startGame;
-	public Button option;
+	public Button[] option;
 	public Diving diving;
 
 	public void OnPointerDown(PointerEventData eventData)
 	{
 		//Debug.Log("Get Touch");
 		gameObject.SetActive(false);
-		option.gameObject.SetActive(false);
+
+		for (int i = 0; i < option.Length; i++)
+        {
+			option[i].gameObject.SetActive(false);
+		}
+		
 		startGame.StartCoroutine("InGame");
 		diving.StartCoroutine("Swim");
 	}
