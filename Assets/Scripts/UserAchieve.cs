06ce0c968c01229c63c6e7be633d2258fddbedc2
Jinsab <b01077016039@gmail.com>
2020-12-02 10:17:28 +0900

    사용자 업적 갯수 체크 스크립트

--- Assets/Scripts/UserAchieve.cs
+++ Assets/Scripts/UserAchieve.cs
@@ -7,6 +7,7 @@ using GooglePlayGames.BasicApi;
 public class UserAchieve : MonoBehaviour
 {
     private int depth; // 플레이어 최대 도달 깊이
+    private int data = 0; // 업적 갯수
 
     // Start is called before the first frame update
     void Start()
@@ -17,46 +18,60 @@ public class UserAchieve : MonoBehaviour
         if (depth >= 200)
         {
             PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_200_meters_over, 2, null);
+            data++;
         }
 
         if (depth >= 500)
         {
             PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_500_meters_over, 3, null);
+            data++;
         }
 
         if (depth >= 800)
         {
             PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_800_meters_over, 4, null);
+            data++;
         }
 
         if (depth >= 1300)
         {
             PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_1300_meters_over, 5, null);
+            data++;
         }
 
         if (depth >= 2000)
         {
             PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_2000_meters_over, 6, null);
+            data++;
         }
 
         if (depth >= 3600)
         {
             PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_3600_meters_over, 7, null);
+            data++;
         }
 
         if (depth >= 6200)
         {
             PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_6200_meters_over, 8, null);
+            data++;
         }
 
         if (depth >= 7800)
         {
             PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_7800_meters_over, 9, null);
+            data++;
         }
 
         if (depth >= 11000)
         {
             PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_11000_meters_over, 10, null);
+            data++;
         }
+    }
+
+    public int getData()
+    {
+        return data;
     }
 }
